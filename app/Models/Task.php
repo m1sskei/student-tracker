1edfb3ae2125a7f951b479e074f4121f586f0152
Kei Agulto <marckagulto@gmail.com>
2026-07-02 13:06:06 +0700

    Added Spatie Activity Logs

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity; // CCTV 1
+use Spatie\Activitylog\LogOptions; // CCTV 2
 
 class Task extends Model
 {
-    protected $fillable = ['user_id', 'title', 'description', 'due_date', 'priority', 'status'];
+    // Idinagdag ang LogsActivity dito
+    use HasFactory, LogsActivity;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'priority',
+        'due_date',
+        'status',
+        'user_id'
+    ];
+
+    // Ito ang magsasabi kung ano ang ire-record
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['title', 'status', 'priority']) // Babantayan niya kapag may nagbago dito
+            ->logOnlyDirty() // Ire-record lang kapag may totoong na-edit
+            ->dontSubmitEmptyLogs();
+    }
 
     public function user()
     {
